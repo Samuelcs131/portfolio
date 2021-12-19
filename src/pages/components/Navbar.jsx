42b5596622ce7bbe2638640848d5e12580918dc9
Samuel Claudino <samuelcs131@gmail.com>
2021-12-19 01:57:50 -0300

    ajuste menu mobile

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -1,28 +1,45 @@
 import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  
 
     useEffect(()=>{
         // GET ELEMENTS HTML
         const GET_ELEMENT = (element) => document.querySelector(element);
+        const NAV_LIST = document.querySelectorAll('.nav-list li')
 
         // ACTIVE MENU MOBILE
         GET_ELEMENT('.mobile-menu').addEventListener('click',actionMenu)
 
         function actionMenu(){
-            GET_ELEMENT('.mobile-menu').classList.toggle("active");
-            GET_ELEMENT('.nav-list').classList.toggle("active");
+            GET_ELEMENT('.mobile-menu').classList.toggle('active');
+            GET_ELEMENT('.nav-list').classList.toggle('active');
 
             // ANIMATION LIST MENU
-            document.querySelectorAll('.nav-list li').forEach(
+            NAV_LIST.forEach(
                 (link,index) => { 
                     link.style.animation ? 
                     (link.style.animation = "") :
                     (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`)
                 })
         }
-    })
 
+        // CLOSE MENU 
+        for(let num = 0; num < NAV_LIST.length; num++){
+            NAV_LIST[num].addEventListener('click', () => {
+                // CLOSE MENU & LIST
+                GET_ELEMENT('.mobile-menu').classList.remove('active');
+                GET_ELEMENT('.nav-list').classList.remove('active');
+                // RESET STYLE ANIMATION
+                for(let x = 0; x < 4; x++){
+                    NAV_LIST[x].style.animation = ""
+                }
+
+            })
+        }
+
+    })
+ 
     return (
         <header className="navbar">
         <nav className="container">
