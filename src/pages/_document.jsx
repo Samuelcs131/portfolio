29c5608601d2ac8c3c493b33a66aa2ddbdcf0ebe
Samuel Claudino <samuelcs131@gmail.com>
2021-12-21 04:44:45 -0300

    ajustes area projetos

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -9,8 +9,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='pt-br'>
-        <Head>
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0"/> */}
+        <Head> 
           {/* TAGS SEO */}
         <meta name="description" content="Calcula investimento"/>
         <meta name="keywords" content="samuel claudino da silva, samuel claudino, samuel silva, samuelcs131, samuelcs131@gmail.com, portfolio samuel claudino"/>
--- src/pages/components/Projects.jsx
+++ src/pages/components/Projects.jsx
@@ -8,17 +8,16 @@ const Projects = () => {
 
                 <div className="container-projects">
 
-                    <div className="project pj-one">
-
+                    <div className="project pj-one"> 
+                    <div className="container-project">
                         <div className="info-project">
                             <h1 className="title-project">DataSelic</h1>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni, temporibus.</p>
+                            <p>Ajuda novos investidores a escolher e simular os melhores investimentos a curto prazo de forma fácil e prática.</p>
 
                             <p className="tecnology-project">
                                 <span>Next.js</span> <span>Bootstrap</span> <span>Axios</span> <span>API Banco Central</span> <span>API IBGE</span> <span>Vercel</span>
                             </p>
                         </div> 
-
                         <div className="icons-project"> 
                             <img src="/img/logo-dataselic-white.svg" alt="logo" width={40}/>
                             <div className="icons-right-project">
@@ -31,8 +30,59 @@ const Projects = () => {
                             </div>
                         </div>
                     </div>
-                   
+                    </div>
+                    <div className="project pj-two"> 
+                    <div className="container-project">
+                        <div className="info-project">
+                            <h1 className="title-project">DataSelic</h1>
+                            <p>Ajuda novos investidores a escolher e simular os melhores investimentos a curto prazo de forma fácil e prática.</p>
+
+                            <p className="tecnology-project">
+                                <span>Next.js</span> <span>Bootstrap</span> <span>Axios</span> <span>API Banco Central</span> <span>API IBGE</span> <span>Vercel</span>
+                            </p>
+                        </div> 
+                        <div className="icons-project"> 
+                            <img src="/img/logo-dataselic-white.svg" alt="logo" width={40}/>
+                            <div className="icons-right-project">
+
+                            <Link href="https://github.com/Samuelcs131/DataSelic"><a target="_blank" rel="noopener">
+                            <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a></Link>
+                            
+                            <Link href="http://data-selic.vercel.app"><a target="_blank" rel="noopener">
+                            <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-external-link"><title>DataSelic</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg></a></Link>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="project pj-three"> 
+                    <div className="container-project">
+                        <div className="info-project">
+                            <h1 className="title-project">DataSelic</h1>
+                            <p>Ajuda novos investidores a escolher e simular os melhores investimentos a curto prazo de forma fácil e prática.</p>
+
+                            <p className="tecnology-project">
+                                <span>Next.js</span> <span>Bootstrap</span> <span>Axios</span> <span>API Banco Central</span> <span>API IBGE</span> <span>Vercel</span>
+                            </p>
+                        </div> 
+                        <div className="icons-project"> 
+                            <img src="/img/logo-dataselic-white.svg" alt="logo" width={40}/>
+                            <div className="icons-right-project">
+
+                            <Link href="https://github.com/Samuelcs131/DataSelic"><a target="_blank" rel="noopener">
+                            <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a></Link>
+                            
+                            <Link href="http://data-selic.vercel.app"><a target="_blank" rel="noopener">
+                            <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-external-link"><title>DataSelic</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg></a></Link>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+             
+ 
                 </div>
+
+             
+
             </div>
         </section>
      );
