6357cd9af625664defb7e5df57cc283dcee1130f
Samuel Claudino <samuelcs131@gmail.com>
2022-02-01 02:27:23 -0300

    adicionado tag meta e logo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,17 @@
-import Head from "next/head";
-import About from "./components/About";
-import Apresentation from "./components/Apresentation";
-import Experience from "./components/Experience";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
+import About from "../components/About";
+import Apresentation from "../components/Apresentation";
+import Experience from "../components/Experience";
+import Footer from "../components/Footer";
+import HeadPage from "../components/HeadPage";
+import Navbar from "../components/Navbar";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
 
 export default function Home() {
  
   return (
       <>
-      <Head> <title>Samuel Claudino - Desenvolvedor Full Stack</title> </Head>
+      <HeadPage titlePage={'Samuel Claudino - Desenvolvedor Full Stack'}/>
       
       <Navbar/>
       <Apresentation/>
