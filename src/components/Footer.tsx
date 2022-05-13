49d390538f1e4bf58632dc221e5a9f07a010bac5
Samuel Claudino <samuelcs131@gmail.com>
2022-05-13 04:03:21 -0300

    incluindo link no rodape

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { FooterContainer } from "../styles/footer";
 import { iFooter } from "../types/components";
 import { EmailIcon, GithubIcon, LinkedInIcon } from "./icons/Icon";
@@ -9,9 +10,15 @@ const Footer = ({dataContact}: iFooter): JSX.Element => {
             <span>Desenvolvido por Samuel Claudino</span>
             
             <div>
+                <Link href={dataContact.linkedin}><a target="_blank" rel="noopener">
                 <LinkedInIcon options={{height: '24', width: '24', fill: 'white'}} />
+                </a></Link>
+                <Link href={dataContact.github}><a target="_blank" rel="noopener">
                 <GithubIcon options={{height: '24', width: '24'}} />
+                </a></Link>
+                <Link href={'mailto:'+dataContact.email}><a target="_blank" rel="noopener">
                 <EmailIcon options={{height: '23', width: '25'}} />
+                </a></Link>
             </div>
         </div>
     </FooterContainer>
